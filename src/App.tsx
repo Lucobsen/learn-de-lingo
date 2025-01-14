0fe00383af041ff323a854cef0279a0aac1b65e8
Luke Jacobsen <luke@Lukes-MacBook-Pro.local>
2025-01-14 17:31:30 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Stack,
+  SvgIcon,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+
+const App = () => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <SvgIcon fontSize="large" component={AutoStoriesIcon} />
+            <Typography variant="h6" sx={{ flexGrow: 1 }} pl={2}>
+              Learn De Lingo
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Container
+        sx={{ display: "flex", marginTop: 5, justifyContent: "center" }}
+      >
+        <Stack direction="column" gap={2}>
+          <Button variant="contained">Add Vocabulary</Button>
+          <Button variant="contained">Test Vocabulary</Button>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default App;
